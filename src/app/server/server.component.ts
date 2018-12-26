86f06787b383055f244d0b3b12a5dfa39cc006d5
Giovanni Borghi <gio.borghi@gmail.com>
2018-12-26 13:22:23 +0100

    two-way data binding examples

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  //styleUrls: ['../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 
-export class ServerComponent {
-  title = 'giovannis-app';
+export class ServerComponent implements OnInit {
+  allowNewServer = false;
+  serverID: string = 'giovannis-server';
+  serverStatus: number = 10;
+  serverCreationStatus: string = "no server was created";
+  serverName: string = "";
+  ngOnInit (){};
+  constructor (){
+    setTimeout (() => {
+      this.allowNewServer = true;
+    }, 3000)
+  };
+
+  getServerStatus() {
+    return this.serverStatus;
+  };
+
+  onCreateServer() {
+    this.serverCreationStatus = "server was created!";
+  };
+
+  onUpdateServerName(event: any){
+    console.log(event);
+    this.serverName = event.target.value;
+  }
 }
