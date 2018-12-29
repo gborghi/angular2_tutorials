0d339f8487c0dc4e312af717d66c05b87df31856
gborghi <gio.borghi@gmail.com>
2018-12-29 23:16:11 +0100

    changes in my-first-app

--- my-first-app/src/app/app-routing.module.ts
+++ my-first-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PicturesComponent } from './components/pictures/pictures.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
 
-const routes: Routes = [];
+const routes : Routes = [
+  { path: 'pictures', component: PicturesComponent },
+  { path: 'servers', component: ServersComponent },
+  { path: 'server', component: ServerComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,21 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './components/server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
+
 import { BasicHighlightDirective } from './directives/basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './directives/better-highlight/better-highlight.directive';
 import { UnlessDirective } from './directives/unless/unless.directive';
 import { AccountService } from './services/account/account.service';
 import { LoggingService } from './services/logging/logging.service';
-import { Routes, RouterModule } from '@angular/router';
+//import { Routes, RouterModule } from '@angular/router';
 import { PicturesComponent } from './components/pictures/pictures.component';
-
-const appRoutes : Routes = [
-  { path: 'pictures', component: PicturesComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'server', component: ServerComponent }
-]
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +29,6 @@ const appRoutes : Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
   ],
   providers: [AccountService, LoggingService],
   bootstrap: [AppComponent]
