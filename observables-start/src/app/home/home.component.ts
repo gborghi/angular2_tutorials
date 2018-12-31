34f66ce532199d0554a8b8ab5360f03ffe086a0c
gborghi <gio.borghi@gmail.com>
2018-12-31 17:40:24 +0100

    forms starting

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable, Observer, Subscription } from 'rxjs';
+//import 'rxjs/Rx';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  numberObsSubsctiption: Subscription;
+  customObsSubscriptions: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    let j=0;
+    const myNumbers = interval(1000).pipe(map(
+      (data: number) => {
+        return data*2;
+      }
+    ));
+    this.numberObsSubsctiption=myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+//        observer.error('this does not work');
+        observer.complete();
+      }, 5000);
+    });
+    this.customObsSubscriptions=myObservable.subscribe(
+      (data: string) => {console.log(data)},
+      (error: string) => {console.log(error)},
+      ()=> {console.log('completed');}
+    );
   }
-
+  ngOnDestroy(){
+    this.numberObsSubsctiption.unsubscribe();
+    this.customObsSubscriptions.unsubscribe();
+  }
 }
